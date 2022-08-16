97c94209ec2519682769718f7b7c575c91f79145
dimasirfandanu <dimasputrairfandanu@gmail.com>
2022-08-16 12:50:33 +0700

    modified:   main.py Migrating to convertapi

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import tempfile
 from zipfile import ZipFile
 import shutil
 import os
-import platform
+import convertapi
+# import platform
 from halo import Halo
-import docx2pdf
+# import docx2pdf
+import envs
 
 # Defining root project directory
 rootDIR = os.path.dirname(os.path.abspath(__file__))
@@ -15,9 +17,9 @@ rootDIR = os.path.dirname(os.path.abspath(__file__))
 workDIR = tempfile.mkdtemp()
 
 # Asking variables
-source = input("Sumber Lowongan?:") 
-company = input("Nama Perusahaan?:")
-position = input("Posisi?:")
+source = input("Sumber Lowongan?: ") 
+company = input("Nama Perusahaan?: ")
+position = input("Posisi?: ")
 
 # Preparing PDF
 with ZipFile("{}/template.docx".format(rootDIR), "r") as workFILES:
@@ -37,12 +39,15 @@ shutil.make_archive(docxOUT, "zip", workDIR)
 os.rename("{}.zip".format(docxOUT), "{}/cv.docx".format(workDIR)) 
 
 workPDF = "{}/cv.pdf".format(workDIR)
-if platform.system() == "Linux":
-    Halo(text="Using LibreOffice to create PDF...", spinner="dots").start()
-    os.system("soffice --convert-to pdf {} --outdir {} &> /dev/null".format(docxOUT, workDIR))
-if platform.system() == "Windows":
-    Halo(text="Using Microsoft Office to create PDF...", spinner="dots").start()
-    docx2pdf.convert(docxOUT, workPDF)
+# if platform.system() == "Linux":
+   #  Halo(text="Using LibreOffice to create PDF...", spinner="dots").start()
+   #  os.system("soffice --convert-to pdf {} --outdir {} &> /dev/null".format(docxOUT, workDIR))
+# if platform.system() == "Windows":
+   #  Halo(text="Using Microsoft Office to create PDF...", spinner="dots").start()
+   #  docx2pdf.convert(docxOUT, workPDF)
+Halo(text="Using convertapi to convert PDF...", spinner="dots").start()
+convertapi.api_secret = envs.convertapisecret
+convertapi.convert('pdf', {'File': docxOUT}, from_format = 'docx').save_files(workDIR)
 shutil.copy2(workPDF, "{}/berkas/CV-Oddy-{}-{}.pdf".format(rootDIR, company, position))
 
 # TODO: Sending email
